a1a49e3374f0e0a216c85aac3a4ab21f69af55aa
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-09 09:08:23 -0400

    value and ref types

--- F24Week2CSharpFundamentals/Program.cs
+++ F24Week2CSharpFundamentals/Program.cs
@@ -25,6 +25,35 @@
 
             // string interpolation
             Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
+
+
+            string[] colors = { "Red", "Blue", "Yellow", "Green", "Pink" };
+
+            foreach(var c in colors)
+                Console.WriteLine($"{c,-10}" + "Hello");
+
+
+            int p = 5;
+            int q = p;
+            p++;
+            Console.WriteLine(p);
+            Console.WriteLine(q);
+
+            Console.WriteLine("\n\n");
+
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.radius);
+
+            Circle c2 = c1;
+            //Circle c2 = new Circle();
+            Console.WriteLine(c2.radius);
+            c2.radius = 30;
+            Console.WriteLine(c1.radius);
         }
+    }
+
+    class Circle
+    {
+        public double radius;
     }
 }
