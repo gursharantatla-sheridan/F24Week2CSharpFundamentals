e95d154a197cf15e69a2cb256ad3266c42b663a4
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-09 08:48:19 -0400

    string interpolation

--- F24Week2CSharpFundamentals/Program.cs
+++ F24Week2CSharpFundamentals/Program.cs
@@ -12,6 +12,19 @@
 
             int i = 44;
             long l = 456543L;
+
+            var x = 34.4;
+
+            Console.WriteLine(@"Hello\nWorld");
+
+
+            int cars = 3, bikes = 5, trucks = 6;
+
+            // string concatenation
+            Console.WriteLine("I have " + cars + " cars, " + bikes + " bikes and " + trucks + " trucks");
+
+            // string interpolation
+            Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
         }
     }
 }
