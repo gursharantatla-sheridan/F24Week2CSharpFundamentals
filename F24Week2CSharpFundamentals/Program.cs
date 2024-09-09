ecc888080af1b96adc6b6c5df04f04c53fe3cbd3
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-09-09 10:16:04 -0400

    complete

--- F24Week2CSharpFundamentals/Program.cs
+++ F24Week2CSharpFundamentals/Program.cs
@@ -49,6 +49,39 @@
             Console.WriteLine(c2.radius);
             c2.radius = 30;
             Console.WriteLine(c1.radius);
+
+            Console.WriteLine("\n\n");
+
+
+
+            string s1 = "hello";
+            string s2 = s1;
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            s1 = "bye";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            Console.WriteLine("\n\n");
+
+
+
+            double amount = 32453.234;
+            Console.WriteLine(amount.ToString("C"));
+
+            double rate = 0.15;
+            Console.WriteLine(rate.ToString("P0"));
+
+            double num = 5434732.456435;
+            Console.WriteLine(num.ToString("N3"));
+
+
+            int? ss = null;
+
+            //int www = num as double;
         }
     }
 
